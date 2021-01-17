8c5388b0159ed0e08f23098f103b6cca300baef0
Zaad-Chima <75208307+Zaad-Chima@users.noreply.github.com>
2021-01-17 08:48:11 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var cartValue = document.getElementById("cart-value");
+var cartButton = document.getElementById("cart");
+
+var book1Add = document.getElementById("book1");
+var book2Add = document.getElementById("book2");
+var book3Add = document.getElementById("book3");
+var book4Add = document.getElementById("book4");
+var book5Add = document.getElementById("book5");
+var book6Add = document.getElementById("book6");
+
+var game1Add = document.getElementById("game1");
+var game2Add = document.getElementById("game2");
+var game3Add = document.getElementById("game3");
+var game4Add = document.getElementById("game4");
+
+var craft1Add = document.getElementById("craft1");
+var craft2Add = document.getElementById("craft2");
+var craft3Add = document.getElementById("craft3");
+var craft4Add = document.getElementById("craft4");
+
+console.log(book1Add);
+console.log(book2Add);
+console.log(book3Add);
+console.log(book4Add);
+console.log(book5Add);
+console.log(book6Add);
+
+console.log(game1Add);
+console.log(game2Add);
+console.log(game3Add);
+console.log(game4Add);
+
+console.log(craft1Add);
+console.log(craft2Add);
+console.log(craft3Add);
+console.log(craft4Add);
